42946a8d372268d4093c95cc383a1b1b4db27083
Caleb Simmons <caleb_simmons>
2022-09-23 19:34:34 -0400

    init gatsby

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,14 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Caleb Simmons Online`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://calebsimmons.online`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass"],
+  pathPrefix: "/csonline"
 };
 
 export default config;
